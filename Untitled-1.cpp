7fde7e5d74afae48a3562267120d9c5c795e34f0
Abdallah Alghannam <71399@students.riphah.edu.pk>
2026-05-18 18:50:51 +0500

    Implement Parcel binary search tree
    Added a binary search tree implementation for Parcel objects, including insert, search, delete, and save to file functionalities.

--- Untitled-1.cpp
+++ Untitled-1.cpp
@@ -109,6 +109,74 @@ public:
 
 // ==========================================
 // --- ABDULLAH CODE HERE ---
+struct TreeNode {
+    Parcel data;
+    TreeNode* left;
+    TreeNode* right;
+};
+
+class ParcelBST {
+public:
+    TreeNode* root;
+    ParcelBST() { root = NULL; }
+    
+    TreeNode* insert(TreeNode* node, Parcel p) {
+        if (node == NULL) {
+            TreeNode* newNode = new TreeNode();
+            newNode->data = p;
+            newNode->left = NULL;
+            newNode->right = NULL;
+            return newNode;
+        }
+        if (p.trackingID < node->data.trackingID) node->left = insert(node->left, p);
+        else if (p.trackingID > node->data.trackingID) node->right = insert(node->right, p);
+        return node;
+    }
+    
+    TreeNode* search(TreeNode* node, int id) {
+        if (node == NULL) { 
+            cout << "Parcel not found in system records!\n"; 
+            return NULL; 
+        }
+        if (node->data.trackingID == id) {
+            string cleanDest = node->data.destination;
+            for(int k = 0; k < cleanDest.length(); k++) {
+                if(cleanDest[k] == '_') cleanDest[k] = ' ';
+            }
+            cout << "Found! ID: " << setfill('0') << setw(4) << node->data.trackingID << setfill(' ')
+                 << " | Dest: " << cleanDest 
+                 << " | Weight: " << node->data.weight << " kg\n";
+            return node;
+        }
+        if (id < node->data.trackingID) return search(node->left, id);
+        else return search(node->right, id);
+    }
+    
+    TreeNode* deleteNode(TreeNode* root, int id) {
+        if (root == NULL) return root;
+        if (id < root->data.trackingID) root->left = deleteNode(root->left, id);
+        else if (id > root->data.trackingID) root->right = deleteNode(root->right, id);
+        else {
+            if (root->left == NULL) { TreeNode* temp = root->right; delete root; return temp; }
+            else if (root->right == NULL) { TreeNode* temp = root->left; delete root; return temp; }
+            TreeNode* temp = root->right;
+            while (temp && temp->left != NULL) temp = temp->left;
+            root->data = temp->data;
+            root->right = deleteNode(root->right, temp->data.trackingID);
+        }
+        return root;
+    }
+    
+    void saveToFile(TreeNode* node, ofstream& file) {
+        if (node != NULL) {
+            saveToFile(node->left, file);
+            file << setfill('0') << setw(4) << node->data.trackingID << setfill(' ') << " " 
+                 << node->data.destination << " " 
+                 << node->data.weight << "\n";
+            saveToFile(node->right, file);
+        }
+    }
+};
 // ==========================================
 
 
